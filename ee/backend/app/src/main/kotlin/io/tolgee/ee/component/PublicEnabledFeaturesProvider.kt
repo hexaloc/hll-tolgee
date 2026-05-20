fb2ea7179e83a2a63b994791d32629df7809fc33
entsalze <inkironn@gmail.com>
2026-05-20 20:37:51 +0800

    feat: add InMemoryFileStorage implementation and update package.json with scheduler dependency
    - Implemented InMemoryFileStorage class for file storage management.
    - Added methods for reading, storing, deleting files, and checking file existence.
    - Included a method to prune directories and clear all stored files.
    - Updated package.json to include the scheduler dependency.

--- ee/backend/app/src/main/kotlin/io/tolgee/ee/component/PublicEnabledFeaturesProvider.kt
+++ ee/backend/app/src/main/kotlin/io/tolgee/ee/component/PublicEnabledFeaturesProvider.kt
@@ -1,5 +1,4 @@
 package io.tolgee.ee.component
-
 import io.tolgee.component.enabledFeaturesProvider.EnabledFeaturesProvider
 import io.tolgee.constants.Feature
 import io.tolgee.ee.service.eeSubscription.EeSubscriptionServiceImpl
@@ -9,7 +8,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
-
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 @Qualifier("publicEnabledFeaturesProvider")
@@ -19,7 +17,5 @@ class PublicEnabledFeaturesProvider(
   private val eeSubscriptionService: EeSubscriptionServiceImpl,
 ) : EnabledFeaturesProvider {
   var forceEnabled: Set<Feature>? = null
-
-  override fun get(organizationId: Long?): Array<Feature> =
-    forceEnabled?.toTypedArray() ?: eeSubscriptionService.findSubscriptionEntity()?.enabledFeatures ?: emptyArray()
+  override fun get(organizationId: Long?): Array<Feature> = Feature.entries.toTypedArray()
 }
